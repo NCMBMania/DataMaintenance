395508cace1a2fc7039a278b961f6ab0d0f141c8
Atsushi Nakatsugawa <atsushi@moongift.jp>
2016-11-18 09:37:48 +0900

    Update

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var application_key = "APPLICATION_KEY";
 var client_key = "CLIENT_KEY";
 var ncmb = new NCMB(application_key, client_key);
+var vm;
+
+var manageRole;
+
+roles = {};
+var ary = ['Administrator', 'Manager'];
+for (i in ary) {
+  ncmb.Role.equalTo("roleName", ary[i])
+    .fetch()
+    .then(function(role) {
+      roles[ary[i]] = role;
+    });
+}
 
 // ユーザが指定したロールに所属しているかチェックします
 ncmb.User.isBelongTo = function(role) {
@@ -57,21 +70,23 @@ var checkRole = function() {
     changeMenu("login");
     return;
   }
-  ncmb.Role.equalTo("roleName", "Administrator")
+  
+  ncmb.Role.equalTo("roleName", 'Administrator')
     .fetch()
-    .then(function (role){
-      ncmb.User.isBelongTo(role)
-    .then(function(result) {
-        if (result) {
-          changeMenu("admin");
-          console.log("所属しています")
-        }else{
-          changeMenu("manager");
-          console.log("所属していません")
-        }
-      }, function(err) {
-        console.log("エラー", err);
-      })
+    .then(function(role) {
+      roles['Administrator'] = role;
+      ncmb.User.isBelongTo(roles['Administrator'])
+        .then(function(result) {
+          if (result) {
+            changeMenu("admin");
+            console.log("所属しています")
+          }else{
+            changeMenu("manager");
+            console.log("所属していません")
+          }
+        }, function(err) {
+          console.log("エラー", err);
+        });
     });
 };
 
@@ -114,20 +129,15 @@ $(function() {
     });
     
     user.signUpByAccount().then(function(user){
-      ncmb.Role.equalTo("roleName", "Manager")
-        .fetch()
-        .then(function (role){
-          role
-            .addUser(user)
-            .update()
-        })
+      roles['Manager']
+        .addUser(user)
+        .update()
         .then(function (role){
           $("#createManagerForm")[0].reset();
           showMessage('success', 'アカウントを追加しました');
         });
     });
-    
-  })
+  });
   
   $("#logout").on("click", function(e) {
     e.preventDefault();
@@ -146,6 +156,96 @@ $(function() {
     $(".page").addClass('hide');
     $(".createManager").removeClass('hide');
   })
-    
+  
+  $(".dataManage").on('click', function(e) {
+    e.preventDefault();
+    $('.page').addClass('hide');
+    $('.dataManagement').removeClass('hide');
+    if (typeof vm == 'object') {
+      return;
+    }
+    var className = $(e.target).data('classname')
+    var ClassData = ncmb.DataStore(className);
+    ClassData.fetchAll()
+      .then(function(results) {
+        if (Object.keys(results).length == 0) {
+          var data = {
+            header: ['objectId'],
+            body: []
+          }
+        } else {
+          var data = {
+            header: [],
+            body: results
+          };
+          for (var i in results) {
+            data.header = data.header.concat(Object.keys(results[i]));
+          }
+          data.header = data.header.filter(function(value, index, self) {
+            return self.indexOf(value) == index;
+          });
+        }
+        var id = 1;
+        vm = new Vue({
+          el: '#maintenance',
+          data: {
+            data: data,
+            className: className
+          },
+          methods: {
+            add_row: function() {
+              var item = new ClassData({_id: id});
+              var acl = new ncmb.Acl;
+              acl.setRoleReadAccess(roles['Manager'], true);
+              acl.setRoleWriteAccess(roles['Manager'], true);
+              acl.setPublicReadAccess(true);
+              item.set('acl', acl);
+              this.data.body.push(item);
+              id++;
+            },
+            add_cloumn: function() {
+              var columnName = prompt("カラム名を入力してください");
+              this.data.header.push(columnName);
+            },
+            delete_row: function(row) {
+              var me = this;
+              if (typeof row.objectId == 'undefined') {
+                this.data.body = this.data.body.filter(function(data) {
+                  return data._id != row._id;
+                });
+                showMessage('success', '削除しました');
+              } else {
+                row.delete()
+                  .then(function() {
+                    me.data.body = me.data.body.filter(function(data) {
+                      return data.objectId != row.objectId;
+                    });
+                    showMessage('success', '削除しました');
+                  })
+              }
+            },
+            done_edit: function(row) {
+              var me = this;
+              if (typeof row.objectId == 'undefined') {
+                delete row['_id'];
+                row.save()
+                  .then(function(obj) {
+                    showMessage('success', '作成しました');
+                    vm.$forceUpdate();
+                  });
+              } else {
+                row.update()
+                  .then(function(obj) {
+                    showMessage('success', '更新しました');
+                    vm.$forceUpdate();
+                  })
+              }
+              
+            }
+          }
+        })
+      })
+  });
+  
   checkRole();
 });
